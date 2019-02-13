46cc5e1cbfa2a4757f7c4f29b30e56d7679454d1
Steven <Steven-ptkhuu@hotmail.com>
2019-02-13 14:58:18 +0100

    project

---
+++ Assets/Scrips/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float posX;
+    float posY;
+    float posZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posX = transform.position.x;
+        posY = transform.position.y;
+        posZ = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void basicMovement()
+    {
+
+    }
+}
